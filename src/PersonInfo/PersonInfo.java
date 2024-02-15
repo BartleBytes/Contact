763be21c7c4ca2aaabe34d5678683b7325d39f46
dylanbartle <dylanbartle@hotmail.com>
2024-02-15 14:01:47 -0700

    "first commit"

---
+++ src/PersonInfo/PersonInfo.java
@@ -0,0 +1,19 @@
+package PersonInfo;
+
+public class PersonInfo {
+    public static void main(String[] args) {
+        // Define variables for the person's information
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "123 Main St";
+        String city = "Anytown";
+        String zipCode = "12345";
+        
+        // Print the person's information
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PersonInfo {
+}
